0703c32df6fa4d03cc567c88d199e6837823d1d5
SuniL <45892241+Serlya@users.noreply.github.com>
2019-02-23 18:30:15 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAIWLVXHT47Y3XPKUA"
-  secret_key = "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"
+  access_key = "xxx"
+  secret_key = "xxx"
   region     = "${var.region}"
 }
